a09cc555277b580055274fe5520765c31df9c2e8
elioenaiferrari <elioenaiferrari@gmail.com>
2021-02-20 09:16:47 -0300

    signin

--- src/main/routes/auth.ts
+++ src/main/routes/auth.ts
@@ -1,9 +1,9 @@
+import { Router } from 'express';
 import { expressRouterAdapter } from '@main/adapters/express-router';
 import {
   makeSigninPresenter,
   makeSignupPresenter,
 } from '@main/factories/presenters/auth';
-import { Router } from 'express';
 
 export default (router: Router): void => {
   router.post('/auth/signup', expressRouterAdapter(makeSignupPresenter()));
